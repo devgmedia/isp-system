e5a186cd72ed6e3454f65542481f8b6df23aa6f3
oppytut <ariefna95@gmail.com>
2022-07-23 23:56:19 +0700

    bad commit

--- database/migrations/2014_10_12_000000_create_user_table.php
+++ database/migrations/2014_10_12_000000_create_user_table.php
@@ -19,7 +19,7 @@ class CreateUserTable extends Migration
             $table->bigIncrements('id');
             $table->string('name')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable()->default(null);
+            $table->timestamp('email_verified_at')->nullable()->default(NULL);
             $table->string('password');
             $table->rememberToken();
             $table->string('api_token', 80)->unique()->nullable()->default(null);
